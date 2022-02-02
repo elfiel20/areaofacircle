d2ea4ccfb3225f934d4d471438c8afb0b7b71538
elfiel20 <98498077+elfiel20@users.noreply.github.com>
2022-02-02 18:34:08 -0500

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,14 @@
+#This is the code for Exercise 4
+#This code is meant to provide the user with the
+#area of a circle
+
+#p is representative of pi
+
+p=3.14159
+
+
+print("This is a program to calculate the area of a circle.")
+
+r= float(input("Enter the radius of the circle :"))
+
+print("The area of the circle is approximately",p*r**2.0)
